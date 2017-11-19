3ddc4dcd4ff153e4684feae85996a95bae6dc75b
Pim de Wit <pim@pdw.io>
2017-11-19 21:36:34 +0000

    feat(challenge): added first challenge

---
+++ 2-javascript/1-setup/javascript/app.js
@@ -0,0 +1,38 @@
+/**
+ * Welcome!
+ * You are currently reading a JavasScript comment! :)
+ * Comments are generally very useful to make notes of what you are doing.
+ * Writing comments on your code is always a good idea!
+ * Mainly so the future you does not get bamboozled by your old code.
+ */
+
+// If your comment is small you can also do it over one line with a double slash (//).
+
+// Great. Let's write some actual javascript.
+
+/**
+ * The absolute first thing you need to get to know is the browser's "Developer Tools".
+ * Developer tools allow you to inspect the contents of your website/app.
+ * e.g you are able to see all HTML elements currently in your document, or you can debug your javascript.
+ * For now we are focusing on the latter. Let's start off with writing a message to the devtools' console.
+ */
+
+console.log('This should appear in the console.');
+
+/**
+ * We just wrote a console log message! Let's open developer tools in your browser of preference.
+ *
+ * CTRL+I for Chrome.
+ * ??? for Firefox.
+ * ??? for Internet Explorer.
+ * ??? for Safari.
+ * ??? for Opera.
+ *
+ * Once the devtools panel is open, click on the "Console" tab. we should now be able to see our message!
+ */
+
+
+/**
+ * Get yourself familiar with devtools!
+ * There are a lot of tabs that will not immediately make sense. And that is okay.
+ */
